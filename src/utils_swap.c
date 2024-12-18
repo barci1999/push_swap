0c3977209abb874d00d0d15857d25f427733a110
barci1999 <pabloalvarezbar@gmail.com>
2024-12-18 15:27:04 +0100

    movimientos sa y sb

--- src/utils_swap.c
+++ src/utils_swap.c
@@ -10,3 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include"pushswap.h"
+
+void sa(t_node **stack_a)
+{
+	if(!stack_a || *stack_a == NULL || (*stack_a)->next == NULL)
+	{
+		return;
+	}
+	t_node *temp_1;
+	t_node *temp_2;
+	temp_1 = *stack_a;
+	temp_2 = temp_1->next;
+	
+	temp_1->next = temp_2->next;
+	*stack_a = temp_2;
+	temp_2->next = temp_1;
+}
+void sb(t_node **stack_b)
+{
+	if(!stack_b || *stack_b == NULL || (*stack_b)->next == NULL)
+	{
+		return;
+	}
+	t_node *temp_1;
+	t_node *temp_2;
+	temp_1 = *stack_b;
+	temp_2 = temp_1->next;
+	
+	temp_1->next = temp_2->next;
+	*stack_b = temp_2;
+	temp_2->next = temp_1;
+}
